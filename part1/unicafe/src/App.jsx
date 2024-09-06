b4835e381db8879924f1ffabef41148061401e7f
Raman7913 <raman7913523@gmail.com>
2024-09-06 14:56:16 +0545

    1.7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
 
     return (
         <>
@@ -11,6 +12,7 @@ function App() {
             <button
                 onClick={() => {
                     setGood((prevGood) => prevGood + 1);
+                    setTotal((prevTotal) => prevTotal + 1);
                 }}
             >
                 good
@@ -18,6 +20,7 @@ function App() {
             <button
                 onClick={() => {
                     setNeutral((prevNeutral) => prevNeutral + 1);
+                    setTotal((prevTotal) => prevTotal + 1);
                 }}
             >
                 neutral
@@ -25,14 +28,22 @@ function App() {
             <button
                 onClick={() => {
                     setBad((prevBad) => prevBad + 1);
+                    setTotal((prevTotal) => prevTotal + 1);
                 }}
             >
                 bad
             </button>
             <h1>statistics</h1>
+
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>
+                average{" "}
+                {total > 0 ? (good * 1 + neutral * 0 + bad * -1) / total : 0}
+            </p>
+            <p>positive {`${total > 0 ? (good * 100) / total : 0}%`}</p>
         </>
     );
 }
