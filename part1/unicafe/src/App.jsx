a9c6e2aba114c46845dc9978841c8594e678e1e5
Raman7913 <raman7913523@gmail.com>
2024-09-06 15:16:06 +0545

    1.10 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,30 +1,42 @@
 import { useState } from "react";
 
+function StatisticsLine(props) {
+    return (
+        <>
+            <p>
+                {props.text} {props.value}
+            </p>
+        </>
+    );
+}
+
 function Statistics(props) {
     if (props.total > 0) {
         return (
             <>
                 <h1>statistics</h1>
 
-                <p>good {props.good}</p>
-                <p>neutral {props.neutral}</p>
-                <p>bad {props.bad}</p>
-                <p>all {props.total}</p>
-                <p>
-                    average{" "}
-                    {props.total > 0
-                        ? (props.good * 1 +
-                              props.neutral * 0 +
-                              props.bad * -1) /
-                          props.total
-                        : 0}
-                </p>
-                <p>
-                    positive{" "}
-                    {`${
+                <StatisticsLine text="good" value={props.good} />
+                <StatisticsLine text="neutral" value={props.neutral} />
+                <StatisticsLine text="bad" value={props.bad} />
+                <StatisticsLine text="total" value={props.total} />
+                <StatisticsLine
+                    text="average"
+                    value={
+                        props.total > 0
+                            ? (props.good * 1 +
+                                  props.neutral * 0 +
+                                  props.bad * -1) /
+                              props.total
+                            : 0
+                    }
+                />
+                <StatisticsLine
+                    text="positive"
+                    value={`${
                         props.total > 0 ? (props.good * 100) / props.total : 0
                     }%`}
-                </p>
+                />
             </>
         );
     }
