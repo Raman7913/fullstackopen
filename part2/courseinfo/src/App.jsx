82bb83f2b4cc6ea8f0c89d2d1042a53def7ec1a9
Raman7913 <raman7913523@gmail.com>
2024-11-11 15:16:56 +0545

    2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,61 +1,68 @@
-const Header = (props) => {
-    const { name } = props;
-    return <h1>{name}</h1>;
-};
+const App = () => {
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
-const Content = ({ parts }) => {
     return (
         <>
-            {parts.map((part) => (
-                <Part key={part.id} part={part} />
+            <h1>Web development curriculum</h1>
+            {courses.map((course) => (
+                <div key={course.id}>
+                    <h2>{course.name}</h2>
+                    {course.parts.map((part) => (
+                        <p key={part.id}>{`${part.name} ${part.exercises}`}</p>
+                    ))}
+                    <h3>
+                        total of{" "}
+                        {course.parts.reduce((sum, part) => {
+                            return part.exercises + sum;
+                        }, 0)}{" "}
+                        exercises
+                    </h3>
+                </div>
             ))}
         </>
     );
 };
-
-const Part = (props) => {
-    return (
-        <>
-            <p>
-                {props.part.name} {props.part.exercises}
-            </p>
-        </>
-    );
-};
-const Course = (props) => {
-    const { id, name, parts } = props.course;
-    return (
-        <>
-            <Header name={name} />
-            <Content parts={parts} />
-        </>
-    );
-};
-
-const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-        ],
-    };
-
-    return <Course course={course} />;
-};
-
 export default App;
